1e71f5fe056be6e8efeef8d7157f69e6d0425917
jagadeesh46723_2b1eaf64566a <jagadeesh46723@gmail.com>
2024-04-12 23:44:01 +0530

    Adding initial changes

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public required string  Name { get; set; }
+        public List<string> Category { get; set; } = [];
+        public required string Description { get; set; }
+        public required string  ImageFile { get; set; }
+        public Decimal Price { get; set; }    
+
+    }
+}
